fe1405469ac3aee3aba1932f041af3e7c426b8f1
HermanL02 <Hermanyiqunliang@gmail.com>
2025-06-23 15:12:50 -0300

    fix: update role of Zhida Liang in AboutSectionTwo component

--- src/components/About/AboutSectionTwo.tsx
+++ src/components/About/AboutSectionTwo.tsx
@@ -16,7 +16,7 @@ const AboutSectionTwo = () => {
     },
     {
       name: "Zhida Liang",
-      role: "Co-Founder & COO",
+      role: "Co-Founder",
       description: "Driving operational excellence and business strategy. Zhida brings extensive experience in business process optimization and client relationship management.",
       image: "/images/testimonials/author-02.png"
     },
